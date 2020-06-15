d8fb0095f2fbe5a1a679291884fedbb97957c856
erik <erik@satellitestud.io>
2020-06-15 17:25:53 +0200

    scratch

--- template/src/data/config.ts
+++
@@ -1,5 +0,0 @@
-export const DEFAULT_WORKSPACE = {
-  latitude: 0,
-  longitude: 0,
-  zoom: 1,
-}
--- template/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type WorkspaceParam = 'zoom' | 'latitude' | 'longitude'
-
-export type QueryParams = {
-  [query in WorkspaceParam]?: string | number | boolean | null
-}
