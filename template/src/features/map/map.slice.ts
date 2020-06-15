baf5ce2b44b0edb2f5b985c8f30ef654cf60a786
erik <erik@satellitestud.io>
2020-06-15 18:01:55 +0200

    added some comments

--- template/src/features/map/map.slice.ts
+++ template/src/features/map/map.slice.ts
@@ -14,13 +14,17 @@ export interface MapState {
 }
 
 const initialState: MapState = {
+  // This is the configuration eventually provided to GFW's Layer Composer in Map.tsx
   generatorsConfig: [
-    { id: 'background', type: Generators.Type.Background },
+    { id: 'background', type: Generators.Type.Background, color: '#ff00ff' },
     { id: 'satellite', type: Generators.Type.Basemap, visible: true },
   ],
 }
 
 export type UpdateGeneratorPayload = { id: string; config: Partial<AnyGeneratorConfig> }
+
+// This slice is the part of the store that handles preparing generators configs, that then get passed
+// to Layer Composer, which generates a style object that can be used by Mapbox GL (https://docs.mapbox.com/mapbox-gl-js/style-spec/)
 export const mapSlice = createSlice({
   name: 'map',
   initialState,
@@ -36,8 +40,12 @@ export const mapSlice = createSlice({
   },
 })
 
+// createSlice generates for us actions that we can use to modify the store by calling dispatch, or using the useDispatch hook
 export const { updateGenerator } = mapSlice.actions
 
+// This is a simple selector that just picks a portion of the stor for consumption by either a component,
+// or a more complex memoized selector. Memoized selectors and/or that need to access several slices,
+// should go into a distiinct [feature].selectors.ts file (use createSelector from RTK)
 export const selectGeneratorsConfig = (state: RootState) => state.map.generatorsConfig
 
 export default mapSlice.reducer
