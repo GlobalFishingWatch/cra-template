bd15c73538494a9c9d26844af5c7e569cb983dab
erik <erik@satellitestud.io>
2020-06-15 18:01:55 +0200

    added some comments

--- template/src/features/map/map.hooks.ts
+++ template/src/features/map/map.hooks.ts
@@ -11,6 +11,8 @@ import {
   UpdateGeneratorPayload,
 } from './map.slice'
 
+// This is a convenience hook that returns at the same time the portions of the store we interested in
+// as well as the functions we need to update the same portions 
 export const useGeneratorsConnect = () => {
   const dispatch = useDispatch()
   return {
